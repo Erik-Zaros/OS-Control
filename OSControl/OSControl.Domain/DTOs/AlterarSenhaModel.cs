de62c9ba7485204381d542b005b14172f4d14b36
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2025-03-12 20:43:54 -0300

    dtos

---
+++ OSControl/OSControl.Domain/DTOs/AlterarSenhaModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OSControl.Domain.DTOs
+{
+    public class AlterarSenhaModel
+    {
+        public long UserId { get; set; }
+        public string SenhaAtual { get; set; }
+        public string NovaSenha { get; set; }
+    }
+}
