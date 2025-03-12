a6cb0d195a3af321301e9d2101225993e7400648
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2025-03-12 19:45:15 -0300

    implementacao hash de senha

---
+++ OSControl/OSControl.Application/Helpers/BCryptHash.cs
@@ -0,0 +1,24 @@
+﻿namespace OSControl.Application.Helpers
+{
+    public class BCryptHash
+    {
+        private const int WorkFactor = 12;
+
+        public string Hash(string senha)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(senha, WorkFactor);
+        }
+
+        public bool Verify(string senha, string hash)
+        {
+            try
+            {
+                return BCrypt.Net.BCrypt.Verify(senha, hash);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
