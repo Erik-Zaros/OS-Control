203f5aea5c1ddc5f7612a42846c219f8e084e5ed
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2025-04-18 21:22:06 -0300

    implementando autenticação

--- FrontEnd/src/boot/axios.js
+++ FrontEnd/src/boot/axios.js
@@ -5,6 +5,14 @@ const api = axios.create({
   baseURL: 'https://oscontrol-fnddbgbmfqcscrdn.brazilsouth-01.azurewebsites.net/api',
 })
 
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios
   app.config.globalProperties.$api = api
