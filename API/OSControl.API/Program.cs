e7b55fcfdb158cd11c7bbef283b74a0f726e8702
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2025-04-18 21:20:30 -0300

    cors

--- API/OSControl.API/Program.cs
+++ API/OSControl.API/Program.cs
@@ -2,6 +2,17 @@ using OSControl.IoC;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 Injection.ConfigureServices(builder.Services, builder.Configuration);
 
 builder.Services.AddControllers();
@@ -17,6 +28,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
