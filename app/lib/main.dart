6892ab6eb813d5e87f4e1ad27dfb499ae1752480
Matheus <matheus2004gabriel.s@users.noreply.github.com>
2025-05-05 05:43:09 -0300

    configuração do multiprovider

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
-import 'routes/app_router.dart';
 import 'package:provider/provider.dart';
-import 'package:teste_flutter_moacir/models/login_form_model.dart';
+import 'routes/app_router.dart';
+import 'models/login_form_model.dart';
 
-void main() => runApp(
-  ChangeNotifierProvider(
-    create: (_) => LoginFormModel(),
-    child: const MyApp(),
-  ),
-);
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<LoginFormModel>(
+          create: (_) => LoginFormModel(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
